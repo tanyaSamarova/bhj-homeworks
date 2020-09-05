33eae8979fe0efe7bc92f446eddb417f5631ef2b
Tanya Samarova <tasha.kostina@yandex.ru>
2020-09-05 14:34:40 +0300

    Задачи 1,3

---
+++ document-structure/tooltip/task.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const hasTooltip = document.querySelectorAll(`.has-tooltip`);
+
+for (let i = 0; i < hasTooltip.length; i++) {
+    hasTooltip[i].insertAdjacentHTML(`afterEnd`, `<div class="tooltip">${hasTooltip[i].title}</div>`);
+
+    hasTooltip[i].addEventListener(`click`, (e) => {
+        e.preventDefault();
+
+        const tooltipList = document.querySelectorAll(`.tooltip`);
+        if (tooltipList[i].classList.contains(`tooltip_active`)) {
+            tooltipList[i].classList.remove(`tooltip_active`);
+        } else {
+            tooltipList[i].classList.add(`tooltip_active`);
+            
+            let place = hasTooltip[i].getBoundingClientRect();
+            
+            tooltipList[i].style.left = place.left + "px";
+            tooltipList[i].style.top = place.top + 20 + "px";
+        }
+    })
+};
